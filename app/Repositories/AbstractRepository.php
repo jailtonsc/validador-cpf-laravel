4af7e06441d1a3dfef70c5cd0cbe50048f4c41fe
Jailton S. C <jailton.dos.santos.cruz@gmail.com>
2015-06-15 21:13:43 -0300

    ajuste de comentario nas funções e atributos

--- app/Repositories/AbstractRepository.php
+++ app/Repositories/AbstractRepository.php
@@ -6,11 +6,18 @@ class AbstractRepository implements RepositoryInterface
 {
     protected $model;
 
+    /**
+     * @return Model
+     */
     public function getTodos()
     {
         return $this->model->all();
     }
 
+    /**
+     * @param array $params
+     * @return Model
+     */
     public function buscar(array $params)
     {
         foreach ($params as $key => $value) {
@@ -22,16 +29,29 @@ class AbstractRepository implements RepositoryInterface
         return $this->model->get();
     }
 
+    /**
+     * @param array $data
+     * @return Model
+     */
     public function salvar(array $data)
     {
         return $this->model->create($data);
     }
 
+    /**
+     * @param Integer $id
+     * @param array $data
+     * @return Model
+     */
     public function editar($id, array $data)
     {
         return $this->model->find($id)->update($data);
     }
 
+    /**
+     * @param Integer $id
+     * @return Model
+     */
     public function deletar($id)
     {
         return $this->model->find($id)->delete();
